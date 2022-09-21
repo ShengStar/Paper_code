af4e0165dc377a849e5ef53d5869f9c437fbe26b
ShengStar <20211401015@cqu.edu.cn>
2022-09-21 12:38:27 +0800

    mlp

--- pcdet/models/backbones_2d/map_to_bev/pointpillar_scatter.py
+++ pcdet/models/backbones_2d/map_to_bev/pointpillar_scatter.py
@@ -12,21 +12,34 @@ class PointPillarScatter(nn.Module):
         assert self.nz == 1
 
         ## mlp
-        self.mlps = nn.ModuleList()
-        shared_mlps = []
-        mlp_spec = [1,64,1]
-        for k in range(len(mlp_spec)-1):
-            shared_mlps.extend([
-                nn.Conv2d(mlp_spec[k], mlp_spec[k+1],
-                        kernel_size=1, bias=False),
-                nn.BatchNorm2d(mlp_spec[k+1]),
-                nn.ReLU()
-            ])
-        self.mlps = nn.ModuleList().append(nn.Sequential(*shared_mlps))
+        # self.mlps = nn.ModuleList()
+        # shared_mlps = []
+        # mlp_spec = [1,64,1]
+        # for k in range(len(mlp_spec)-1):
+        #     shared_mlps.extend([
+        #         nn.Conv2d(mlp_spec[k], mlp_spec[k+1],
+        #                 kernel_size=1, bias=False),
+        #         nn.BatchNorm2d(mlp_spec[k+1]),
+        #         nn.ReLU()
+        #     ])
+        # self.mlps = nn.ModuleList().append(nn.Sequential(*shared_mlps))
         # mlp end
 
+        # mlp
+        # self.mlps = nn.ModuleList()
+        # shared_mlps = []
+        # shared_mlps.extend([nn.Conv2d(64, 64,kernel_size=1, bias=False),nn.BatchNorm2d(64),nn.ReLU()])
+        # self.mlps = nn.ModuleList().append(nn.Sequential(*shared_mlps))
+        # mlp end
+
+
         # topk
-        self.topk = nn.Linear(64, 1, bias=True)
+        self.topk = nn.Linear(64, 64, bias=True)
+        self.nm = nn.BatchNorm1d(64)
+        self.rl = nn.ReLU()
+        self.topk_score = nn.Linear(64, 1, bias=True)
+        self.nm_score = nn.BatchNorm1d(1)
+        self.rl_score = nn.ReLU()
         # topk
 
 
@@ -47,8 +60,17 @@ class PointPillarScatter(nn.Module):
             flag_mask = this_coords[:,4] != -1
             indices = this_coords[:, 1] + this_coords[:, 2] * self.nx + this_coords[:, 3] # torch.Size([8032])
             indices = indices.type(torch.long)
-            print(pillar_features.shape)
+            # print(pillar_features.shape)
             pillars = pillar_features[batch_mask, :]
+            pillars = self.topk(pillars)
+            pillars = self.nm(pillars)
+            pillars = self.rl(pillars)
+
+            score = self.topk_score(pillars)
+            score = self.nm_score(score)
+            score = self.rl_score(score)  
+
+            # print(pillars.shape)
             pillars = pillars.t()
             indices = indices[flag_mask]
             pillars = pillars[:,flag_mask] # 可能出错
